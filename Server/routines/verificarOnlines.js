3ebb6df35ae6d16085f540a33e95d3832b8397ad
William Gerhard <william97623074@gmail.com>
2023-03-28 12:27:36 -0300

    Final

--- Server/routines/verificarOnlines.js
+++ Server/routines/verificarOnlines.js
@@ -3,10 +3,9 @@ const fs = require("fs")
 const colaboradoresPath = path.join(__dirname, "..", "Config", "Colaboradores.json")
 
 function verificarOnlines(){
-    console.log('Lendo arquivo...')
+    console.log('Atualizando Colaboradores Online...')
     const json = JSON.parse(fs.readFileSync(colaboradoresPath))
 
-    console.log('Filtrando colaboradores offline...')
     const colaboradoresofflines = json.filter((colaborador)=>{
         
         const entrada_1 = colaborador.entrada_1
@@ -22,19 +21,12 @@ function verificarOnlines(){
         } 
     })
 
-    console.log('Filtrando colaboradores online...')
     const colaboradoresOnlines = json.filter((colaborador)=>{
 
         const entrada_1 = colaborador.entrada_1
         const saida_1 = colaborador.saida_1
         const entrada_2 = colaborador.entrada_2
         const saida_2 = colaborador.saida_2
-
-        console.log(`Colaborador ${colaborador.nome}:`)
-        console.log(`Entrada 1: ${entrada_1}`)
-        console.log(`Saída 1: ${saida_1}`)
-        console.log(`Entrada 2: ${entrada_2}`)
-        console.log(`Saída 2: ${saida_2}`)
         
         if (verificarHoraAtualNoIntervalo(entrada_1, saida_1)){
             colaborador.ativo_ate = saida_1
@@ -46,39 +38,48 @@ function verificarOnlines(){
 
     })
     
-    console.log('Atualizando colaboradores offline...')
     colaboradoresofflines.forEach((colaborador)=>{
         colaborador.online = false
     })
 
-    console.log('Atualizando colaboradores online...')
     colaboradoresOnlines.forEach((colaborador)=>{
         colaborador.online = true
     })    
 
-    console.log('Escrevendo arquivo...')
     fs.writeFileSync(colaboradoresPath, JSON.stringify(json))
 
-    console.log('Concluído!')
+    console.log(`Atualização concluída!`);
+
 };
 
 function verificarHoraAtualNoIntervalo(horaInicial, horaFinal) {
-  const horaAtual = new Date();
-  const [horaIni, minIni] = horaInicial.split(':');
-  const [horaFim, minFim] = horaFinal.split(':');
+    
+    const horaAtual = new Date();
+    const [horaIni, minIni] = horaInicial.split(':');
+    const [horaFim, minFim] = horaFinal.split(':');
+  
+    // Subtrai 30 minutos da hora final
+    let minutosFim = parseInt(minFim, 10) - 30;
+    let horasFim = parseInt(horaFim, 10);
+  
+    // Verifica se os minutos resultantes são negativos e ajusta as horas, se necessário
+    if (minutosFim < 0) {
+      minutosFim += 60;
+      horasFim -= 1;
+    }
+    
+    const horaInicialFormatada = new Date();
+    horaInicialFormatada.setHours(horaIni, minIni, 0);
 
-  horaInicial = new Date();
-  horaInicial.setHours(horaIni, minIni, 0);
-
-  horaFinal = new Date();
-  horaFinal.setHours(horaFim, minFim, 0);
-
-  const tempoAtual = horaAtual.getTime();
-  const tempoInicial = horaInicial.getTime();
-  const tempoFinal = horaFinal.getTime();
-
-  return (tempoAtual >= tempoInicial && tempoAtual <= tempoFinal);
-}
+    const horaFinalReduzida = new Date();
+    horaFinalReduzida.setHours(horasFim, minutosFim, 0);
+    
+    const tempoAtual = horaAtual.getTime();
+    const tempoInicial = horaInicialFormatada.getTime();
+    const tempoFinal = horaFinalReduzida.getTime();
+  
+    return (tempoAtual >= tempoInicial && tempoAtual <= tempoFinal);
+  }
 
 
 module.exports = verificarOnlines
