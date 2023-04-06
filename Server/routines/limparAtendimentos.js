68f7204a8e9092da102997b37501b381c821b335
William Gerhard <william97623074@gmail.com>
2023-04-06 10:49:05 -0300

    Database Included

--- Server/routines/limparAtendimentos.js
+++ Server/routines/limparAtendimentos.js
@@ -1,35 +1,87 @@
 const path = require("path")
 const fs = require("fs")
+const { log } = require("console")
 const colaboradoresPath = path.join(__dirname, "..", "Config", "Colaboradores.json")
 
 function limparAtendimentos() {
 
-    if (!verificarMeiaNoite()) { return }
+    if (verificarMeiaNoite()) { log(`Não é meia-noite!`); return }
+
+    if (verificarSeJaFoiLimpo()){ log(`Atendimentos Já foram Limpos!`); return }
     
-    const colaboradores = JSON.parse(fs.readFileSync(colaboradoresPath))
-
-    colaboradores.forEach(colaborador => {
-
-        colaborador.total_atendimentos = 0
-        colaborador.total_vendas = 0
-
-    });
-
     console.clear()
+    log(`Limpando atendimentos!`)
 
-    fs.writeFileSync(colaboradoresPath, JSON.stringify(colaboradores))
+    RegistrarAtendimentos()
+
+    Limpar()
 
     console.log(`Atendimentos Limpos`)
-
+    
 }
 
 function verificarMeiaNoite() {
     const agora = new Date();
-    if (agora.getHours() === 0 && agora.getMinutes() === 0 && agora.getSeconds() === 0) {
+    if (agora.getHours() === 1 && agora.getMinutes() === 1) {
         return true
     } else {
         return false
     }
+}
+
+function Limpar(){
+    
+    const colaboradores = JSON.parse(fs.readFileSync(colaboradoresPath))
+    
+    colaboradores.forEach(colaborador => {
+
+        colaborador.total_atendimentos = 0
+        colaborador.total_vendas = 0
+        
+    });
+
+    fs.writeFileSync(colaboradoresPath, JSON.stringify(colaboradores))
+    
+}
+
+function RegistrarAtendimentos(){
+
+    const colaboradores = JSON.parse(fs.readFileSync(colaboradoresPath))
+
+    criarDiretorio()
+
+    fs.writeFileSync(path.join(__dirname, "..", "Config", "backups", buscarDataAtual(), "Colaboradores.json"), JSON.stringify(colaboradores))
+
+}
+
+function criarDiretorio(){
+
+    if(!fs.existsSync(path.join(__dirname, "..", "Config", "backups", buscarDataAtual()))) {
+        fs.mkdirSync(path.join(__dirname, "..", "Config", "backups", buscarDataAtual()))
+    }
+
+}
+
+function buscarDataAtual() {
+
+    const date = new Date()
+
+    const day = date.getDate().toString().padStart(2, 0)
+    const month = (date.getMonth() + 1).toString().padStart(2, 0)
+    const year = date.getFullYear().toString()
+
+    return `${day}-${month}-${year}`
+
+}
+
+function verificarSeJaFoiLimpo(){
+
+    if (fs.existsSync(path.join(__dirname, "..", "Config", "backups", buscarDataAtual()))){
+        return true
+    } else {
+        return false
+    }
+
 }
 
 module.exports = { limparAtendimentos }
